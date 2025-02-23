478cb787c2ddb3cb162500fa53102a8cf1b3d9d1
Carsten <clu@rts.dk>
2025-02-23 21:14:12 +0100

    change way to extract id from route to work on netlify

--- src/app/api/homes/[id]/route.ts
+++ src/app/api/homes/[id]/route.ts
@@ -1,8 +1,15 @@
 import { type NextRequest } from "next/server"
 
-export async function GET(request: NextRequest, { params }: { params: { id: string }}) {
+export async function GET(request: NextRequest) {
     
-  const { id } = await params
+  // Extracts `id` from the URL
+  // netlify functions do not support dynamic routes
+  // so we have to extract the id from the URL
+    const id = request.nextUrl.pathname.split("/").pop(); // Extracts `id` from the URL  
+    
+    if (!id) {
+      return Response.json({ error: "ID parameter is missing" }, { status: 400 });
+    }
     
     const res = await fetch(`https://dinmaegler.onrender.com/homes/${id}`)
     const data = await res.json()
