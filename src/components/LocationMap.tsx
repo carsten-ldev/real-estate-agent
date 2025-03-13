420b033d0e4d210e610c343ffb742a18e761df8d
Carsten <clu@rts.dk>
2025-03-13 18:14:58 +0100

    fixed map scaling

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -1,21 +1,46 @@
 "use client";
-
-import Map, {Marker} from "react-map-gl/mapbox"
+import { useEffect, useRef } from "react";
+import Map, {Marker, MapRef} from "react-map-gl/mapbox"
 import { HiMapPin } from "react-icons/hi2";
 
 export default function LocationMap({ mapboxToken, lat, long }: { mapboxToken: string,  lat: number, long: number }) {
+    const mapRef = useRef<MapRef | null>(null);
+    const containerRef = useRef<HTMLDivElement | null>(null);
+
+
+  useEffect(() => {
+
+    if (!containerRef.current) return;
     
+    const observer = new IntersectionObserver(([entry]) => {
+      if(entry.isIntersecting) {
+        console.log('Map is visible');
+        if (mapRef.current) {
+          setTimeout(() => {
+            mapRef.current?.resize();
+          }, 5); // Small delay to allow layout to settle
+        }
+
+      }
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => observer.disconnect();
+    
+  }, []);
 
     return (
+        <div ref={containerRef} style={{ width: "100%", height: "100%" }}>
         <Map
+            ref={mapRef}
             mapboxAccessToken={ mapboxToken }
+            style={{width: '100%', height: '100%', margin: 0, padding: 0}}
             initialViewState={{
-                
                 longitude: long,
                 latitude: lat,
                 zoom: 13.5
             }}
-            style={{position: 'relative', width: '100%', height: '100%'}}
             mapStyle="mapbox://styles/mapbox/streets-v12"
             
         >
@@ -23,6 +48,7 @@ export default function LocationMap({ mapboxToken, lat, long }: { mapboxToken: s
                 <HiMapPin size={48} color='green'/>
             </Marker>
         </Map> 
+        </div>
     )
 
 }
--- src/components/PopOver.tsx
+++ src/components/PopOver.tsx
@@ -6,7 +6,7 @@ type PopOverProps = {
 
 export default function PopOver({ children, target }: PopOverProps) {
     return (
-     <div id={target} popover="auto" className="p-8 relative  w-[85vw] h-[85vh] m-auto">
+     <div id={target} popover="auto" className="relative w-[85vw] h-[600px] m-auto">
         {children}
         <button popoverTarget={target} popoverTargetAction="hide" className="text-4xl absolute top-0 right-2" >&times;</button>
     </div>
